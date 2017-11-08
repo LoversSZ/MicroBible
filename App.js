fbaa390b08cdc62e436129e050282344b292b599
xiaojiez <xiaojiez@37degree.com>
2017-11-08 10:39:44 +0800

    text

--- App.js
+++ App.js
@@ -24,10 +24,10 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          hello xiaojiez!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          You are beautiful!
         </Text>
         <Text style={styles.instructions}>
           {instructions}
